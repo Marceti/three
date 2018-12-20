4ecec6feb4a3e0ee18ea3dd5bde7de8e982053e8
vagrant <vagrant@three.dev>
2018-12-20 14:42:23 +0000

    begin to work with tests

---
+++ tests/Unit/Authentication/AuthUnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Authentication;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthUnitTest extends TestCase {
+
+  //TODO Laracasts Testing Laravel video 6 - vezi cum utilizezi exceptii , si daca mai este valabil
+
+
+
+}
